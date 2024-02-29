feede8e548b90918c346ff832154109655c7649d
Miguel Domingos <109294009+domingosmiguel@users.noreply.github.com>
2024-02-29 17:12:37 -0300

    initial commit

---
+++ src/authServer.js
@@ -0,0 +1,64 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
+
+  if (refreshToken === undefined) return res.sendStatus(401);
+
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const data = genDataForAccessToken(user.name);
+    const accessToken = generateAccessToken(data);
+    res.json({ accessToken });
+  });
+});
+
+app.delete('/logout', (req, res) => {
+  console.log(req.body);
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+app.post('/login', (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username;
+  const data = genDataForAccessToken(username);
+  const accessToken = generateAccessToken(data);
+  const refreshToken = generateRefreshToken(data);
+
+  refreshTokens.push(refreshToken);
+
+  res.json({ accessToken, refreshToken });
+});
+
+function genDataForAccessToken(name) {
+  return { name, scope: process.env.SCOPE };
+}
+
+function generateAccessToken(data) {
+  return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5m' });
+}
+
+function generateRefreshToken(user) {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '4h' });
+}
+
+const PORT = process.env.AUTH_PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Authentication server running on port ${PORT}`);
+});
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const posts = [
+  {
+    username: 'Miguel',
+    title: 'Post 1',
+  },
+  {
+    username: 'Test',
+    title: 'Post 2',
+  },
+];
+
+app.get('/posts', authenticateToken, (req, res) => {
+  const username = req?.user?.name;
+  console.log(req.user);
+  res.json(posts.filter((post) => post.username === username));
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.split(' ')[1];
+
+  if (token === undefined) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    if (err || data.scope !== process.env.SCOPE) return res.sendStatus(403);
+
+    req.user = data;
+    next();
+  });
+}
+
+app.get('/hello', (req, res) => {
+  res.sendStatus(418);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
